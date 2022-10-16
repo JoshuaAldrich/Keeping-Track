78b010bec155804635e5b6eafca38304d6f9643b
JoshuaAldrich <105825492+JoshuaAldrich@users.noreply.github.com>
2022-10-16 11:30:11 -0400

    Changes made to database and index file to try and get it working

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
   user: "root",
   // Your MySQL password
   password: "imjosh123!",
-  database: "election",
+  database: "department",
 });
 
 module.exports = db;
--- index.js
+++ index.js
@@ -2,27 +2,97 @@ const inquirer = require("inquirer");
 const db = require("./db/connection");
 const sql = require("mysql2");
 const cTable = require("console.table");
+const {
+  getDepartments,
+  addDepartments,
+  deleteDepartment,
+} = require("./lib/department");
+const {
+  getEmployees,
+  addEmployees,
+  updateRole,
+  deleteEmployee,
+} = require("./lib/employee");
+const { allRoles, addRole } = require("./lib/roles");
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+const start = (answers) => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "options",
+        message: "select an option",
+        choices: [
+          "SHOW DEPARTMENTS",
+          "SHOW ROLES",
+          "SHOW EMPLOYEES",
+          "ADD DEPARTMENT",
+          "ADD ROLE",
+          "ADD EMPLOYEE",
+          "UPDATE EMPLOYEE`S ROLE",
+          "DELETE EMPLOYEE",
+          "DELETE DEPARTMENT",
+        ],
+      },
+    ])
+    .then((answers) => {
+      console.log(answers.options);
+      switch (answers.options) {
+        case "SHOW DEPARTMENTS":
+          getDepartments().then(() => {
+            start();
+          });
+          break;
+        case "SHOW ROLES":
+          allRoles().then(() => {
+            start();
+          });
+          break;
+        case "SHOW EMPLOYEES":
+          getEmployees().then(() => {
+            start();
+          });
+          break;
+        case "ADD DEPARTMENT":
+          addDepartments().then(() => {
+            getDepartments();
+            start();
+          });
+          break;
+        case "ADD ROLE":
+          addRole().then(() => {
+            allRoles();
+            start();
+          });
+          break;
+        case "ADD EMPLOYEE":
+          addEmployees().then(() => {
+            getEmployees();
+            start();
+          });
+          break;
+        case "UPDATE EMPLOYEE`S ROLE":
+          updateRole().then(() => {
+            getEmployees();
+            start();
+          });
+          break;
+        case "DELETE EMPLOYEE":
+          deleteEmployee().then(() => {
+            getEmployees();
+            start();
+          });
+          break;
+        case "DELETE DEPARTMENT":
+          deleteDepartment().then(() => {
+            getDepartments();
+            start();
+          });
+          break;
+      }
+    });
+};
 
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+start();
 
-// Use apiRoutes
-app.use("/api", apiRoutes);
-
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-});
-
-// Start server after DB connection
-db.connect((err) => {
-  if (err) throw err;
-  console.log("Database connected.");
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-});
+module.exports = { start };
